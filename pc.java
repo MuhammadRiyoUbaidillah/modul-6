4cad1c34b141994985a0e2a4e21f9a90606bf515
MuhammadRiyoUbaidillah <82527896+MuhammadRiyoUbaidillah@users.noreply.github.com>
2021-06-03 21:44:39 +0700

    Create pc.java

---
+++ pc.java
@@ -0,0 +1,34 @@
+package interfase;
+
+public class pc extends komputer implements mouse, keyboard, printer{
+    
+    @Override
+    void hidupkan_os(){
+        System.out.println("Hidupakn PC");
+    }
+    
+    @Override
+    void matikan_os(){
+        System.out.println("Matikan Os");
+    }
+    
+    @Override
+    public void klik_kanan(){
+        System.out.println("Klik Kanan");
+    }
+    
+    @Override
+    public void klik_kiri(){
+        System.out.println("Klik Kiri");
+    }
+    
+    @Override
+    public void tekan_enter(){
+        System.out.println("Tekan Enter");
+    }
+    
+    @Override
+    public void cetak_data(){
+        System.out.println("Cetak Data");
+    }    
+}
